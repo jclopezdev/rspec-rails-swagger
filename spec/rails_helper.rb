341cf469520f27474546a1356873286110136e16
Juan Carlos López <jcarloslopezl@hotmail.com>
2018-02-27 19:11:25 -0600

    Add database_cleaner configuration in rails_helper file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,17 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
