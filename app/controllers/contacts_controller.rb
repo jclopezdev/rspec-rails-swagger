09ddf09d1337b9369faae03c4f6cf3246e5e7d32
Juan Carlos López <jcarloslopezl@hotmail.com>
2018-03-14 23:52:17 -0600

    Add create method and tests

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,16 @@
 class ContactsController < ApplicationController
   def index
     response = Contact.all
-
     render json: response, status: :ok
   end
+
+  def create
+    response = Contact.create(create_contact_params)
+    render json: response, status: :created
+  end
+
+  private
+    def create_contact_params
+      params.require(:data).permit(:name, :phone, :email, :address)
+    end
 end
--- spec/requests/contacts_request_spec.rb
+++ spec/requests/contacts_request_spec.rb
@@ -20,5 +20,31 @@ RSpec.describe 'Contacts', type: :request, capture_examples: true do
         end
       end
     end
+
+    post(summary: 'Create a new contact') do
+      consumes 'application/json'
+      produces 'application/json'
+      tags :contacts
+
+      parameter :data,
+                in: :body,
+                required: true,
+                schema: {
+                  '$ref' => '#/definitions/createContact'
+                }
+
+      response(201, description: 'Contact created') do
+        let(:data) do
+          {
+            data: {
+              name: 'Magmalabs.io',
+              phone: '+52 1 667 317 9035',
+              email: 'hello@magmalabs.io',
+              address: 'Av. Constitución #2035. Colima, Colima, MX, 28017'
+            }
+          }
+        end
+      end
+    end
   end
 end
